18d1d39582c2d8526e4f93e207c0e3e6ee23282c
Chris Surline <chris.surline@gmail.com>
2024-11-29 17:39:48 -0500

    adding content pack name parsing tool

--- parseContentNames.py
+++ parseContentNames.py
@@ -1,5 +1,7 @@
 # this isn't really an xsoar automation but useful for grabbing all of the names for content in a content pack 
 # e.g. you use demisto-sdk to create a custom content pack and you just want to dump the names for documentation or whatever ;)
+# how I use it: navigate to your content pack (Packs/Content-Pack), run the script `python3 \home\chris\parseContentNames.py`
+# out spits a file with the names (content-names.txt). I also added that filename name to .gitignore. 
 
 import os
 import json
